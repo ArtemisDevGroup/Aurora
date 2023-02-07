00a2c6619c9e1f95d4e05a29ac56dac9c9b14433
Douglas Lyman <douglas.lyman@icloud.com>
2023-02-07 23:47:09 +0100

    Did more on console declarations.

--- Aurora/Console.h
+++ Aurora/Console.h
@@ -5,6 +5,7 @@
 #include "Interfaces.h"
 #include "String.h"
 #include "Exceptions.h"
+#include "Color.h"
 
 #include <Windows.h>
 
@@ -13,6 +14,19 @@ namespace Aurora {
 	public:
 		Console();
 	};
+
+	/// <summary>
+	/// <para>Sets the foreground color of the console. This function only works with applications ran in a TrueColor enabled terminal.</para>
+	/// <para>For example: Windows Terminal is TrueColor enabled, whereas the Legacy Windows Command Prompt is not.</para>
+	/// </summary>
+	/// <param name="TrueColor">- The color to set the foreground to, in RGB.</param>
+	A_VOID SetConsoleForegroundColor(_In_ const RGB& TrueColor);
+	A_VOID ResetConsoleForegroundColor();
+
+	A_VOID SetConsoleBackgroundColor(_In_ const RGB& TrueColor);
+	A_VOID ResetConsoleBackgroundColor();
+
+	A_VOID SetConsoleColorLegacy(_In_ A_WORD wColor);
 }
 
 #endif // !__AURORA_CONSOLE_H__
