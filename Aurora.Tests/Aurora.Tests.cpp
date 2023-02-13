e9d3aa6705867c0831ff35b2409186fe769fedb7
Douglas Lyman <douglas.lyman@icloud.com>
2023-02-13 23:19:29 +0100

    Added a ToDo.

--- Aurora.Tests/Aurora.Tests.cpp
+++ Aurora.Tests/Aurora.Tests.cpp
@@ -1,40 +1,31 @@
 #include <stdio.h>
 
-#include <Aurora/String.h>
-#include <Aurora/Color.h>
-#include <Aurora/Console.h>
-
-using namespace Aurora;
-
-#include <Windows.h>
-
-#define PFUNC __PRETTY_FUNCTION__
-
-void C() {
-	AuroraContextStart();
-	AuroraThrow(ParameterInvalidException, "Test");
-	AuroraContextEnd();
-}
-
-void B() {
-	AuroraContextStart();
-	C();
-	AuroraContextEnd();
-}
-
-void A() {
-	AuroraContextStart();
-	B();
-	AuroraContextEnd();
-}
+//------------------------
+// TODO:
+// All files:
+//		Add copyright and author notice to the top of every file.
+//		Inline document everything.
+// Array.h: Fix SAL and noexcept annotations.
+// Binary.h: --||-- && add ROTL.
+// Color.h:	--||--
+// Console.h: --||-- && Finish decl and def of all functions.
+// Definitions.h: Global.
+// Enumerations.h: Fix noexcept annotations.
+// Events.h: --||--
+// Exceptions.h: Global.
+// Identification.h: Global.
+// Interfaces.h: Take a look into if it is needed in the first place.
+// String.h: Fix everything.
+// Thread.h: Test.
+// Vector.h: Global.
+// 
+// Color.cpp: Same as h.
+// Console.cpp: Same as h.
+// Exceptions.cpp: Global.
+// Identification.cpp: Global.
+// String.cpp: Fix everything.
+//------------------------
 
 int main() {
-	try {
-		A();
-	}
-	catch (ParameterInvalidException& e) {
-		printf("Trace:\n");
-		for (A_I32 i = 0; i < e.GetFunctionCount(); i++)
-			printf("at %s\n", e.GetFunctions()[i]);
-	}
+
 }
