95d7b665f885f77313f78044d35637f7e1583a04
Douglas Lyman <douglas.lyman@icloud.com>
2023-02-23 22:19:11 +0100

    Did more on pipes.

--- Aurora/Pipe.cpp
+++ Aurora/Pipe.cpp
@@ -1,14 +1,114 @@
 #include "Pipe.h"
 
+#include <stdio.h>
+
 namespace Aurora {
+	INamedPipeBase::INamedPipeBase(_In_ LPHANDLE lpPipeHandle) : phPipe(lpPipeHandle) {}
+
+	A_VOID INamedPipeBase::Read(
+		_Out_writes_bytes_(dwNumberOfBytesToRead) A_LPVOID lpBuffer,
+		_In_ A_DWORD dwNumberOfBytesToRead,
+		_Out_opt_ A_LPDWORD lpNumberOfBytesRead = nullptr
+	) {
+		AuroraContextStart();
+
+		if (!ReadFile(
+			*phPipe,
+			lpBuffer,
+			dwNumberOfBytesToRead,
+			lpNumberOfBytesRead,
+			nullptr
+		)) AuroraThrow(WindowsApiException, "ReadFile");
+
+		AuroraContextEnd();
+	}
+
+	A_VOID INamedPipeBase::Read(
+		_Out_writes_z_(dwSize) A_LPSTR lpString,
+		_In_ A_DWORD dwSize
+	) {
+		AuroraContextStart();
+
+		A_LPSTR lpBuffer = new A_CHAR[dwSize];
+		A_DWORD dwNumberOfBytesRead;
+
+		if (!ReadFile(
+			*phPipe,
+			lpBuffer,
+			dwSize,
+			&dwNumberOfBytesRead,
+			nullptr
+		)) {
+			delete[] lpBuffer;
+			AuroraThrow(WindowsApiException, "ReadFile");
+		}
+
+		try { // Try-catch in order to not write null terminator twice.
+			for (A_I32 i = 0; i < dwNumberOfBytesRead - 1; i++) {
+				lpString[i] = lpBuffer[i];
+				if (lpBuffer[i] == '\0') throw 0;
+			}
+
+			lpString[dwNumberOfBytesRead - 1] = '\0';
+		}
+		catch (int) {}
+
+		AuroraContextEnd();
+	}
+
+	A_VOID INamedPipeBase::Write(
+		_In_reads_bytes_(dwNumberOfBytesToWrite) A_LPCVOID lpBuffer,
+		_In_ A_DWORD dwNumberOfBytesToWrite,
+		_Out_opt_ A_LPDWORD lpNumberOfBytesWritten = nullptr
+	) {
+		AuroraContextStart();
+
+		if (!WriteFile(
+			*phPipe,
+			lpBuffer,
+			dwNumberOfBytesToWrite,
+			lpNumberOfBytesWritten,
+			nullptr
+		)) AuroraThrow(WindowsApiException, "WriteFile");
+
+		AuroraContextEnd();
+	}
+
+	A_VOID INamedPipeBase::Write(_In_z_ A_LPCSTR lpString) {
+		AuroraContextStart();
+
+		if (!WriteFile(
+			*phPipe,
+			lpString,
+			strlen(lpString) + 1,
+			nullptr,
+			nullptr
+		)) AuroraThrow(WindowsApiException, "WriteFile");
+
+		AuroraContextEnd();
+	}
+
+	A_VOID INamedPipeBase::Clone(_Reserved_ A_LPVOID) const { /* throw NotImplementedException */ }
+
+	A_VOID INamedPipeBase::Release() {
+		if (*phPipe) {
+			CloseHandle(*phPipe);
+			*phPipe = nullptr;
+		}
+	}
+
+	INamedPipeBase::~INamedPipeBase() { Release(); }
+
 	NamedPipeServer::NamedPipeServer(
 		_In_z_ A_LPCSTR lpName,
 		_In_ PipeOpenMode dwOpenMode,
 		_In_ PipeModeFlags dwPipeMode,
 		_In_range_(1, PIPE_UNLIMITED_INSTANCES) A_DWORD dwMaxInstances = PIPE_UNLIMITED_INSTANCES,
-		_In_ LPSECURITY_ATTRIBUTES lpSecurityAttributes = nullptr
-	) : dwOpenMode(dwOpenMode), dwPipeMode(dwPipeMode), dwMaxInstances(dwMaxInstances), hPipe(nullptr) {
-		strcpy_s(szName, lpName);
+		_In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes = nullptr
+	) : INamedPipeBase(&hPipe), dwOpenMode(dwOpenMode), dwPipeMode(dwPipeMode), dwMaxInstances(dwMaxInstances), hPipe(nullptr) {
+		if (lpName[0] == '\\' && lpName[1] == '\\' && lpName[2] == '.' && lpName[3] == '\\') strcpy_s(szName, lpName);
+		else sprintf_s(szName, "\\\\.\\%s", lpName);
+
 		if (lpSecurityAttributes) {
 			memcpy(&SecurityAttributes, lpSecurityAttributes, sizeof(SECURITY_ATTRIBUTES));
 			bSecurityAttributesActive = true;
@@ -47,95 +147,52 @@ namespace Aurora {
 		AuroraContextEnd();
 	}
 
-	A_VOID NamedPipeServer::Read(
-		_Out_writes_bytes_(dwNumberOfBytesToRead) A_LPVOID lpBuffer,
-		_In_ A_DWORD dwNumberOfBytesToRead,
-		_Out_opt_ A_LPDWORD lpNumberOfBytesRead = nullptr
-	) {
-		AuroraContextStart();
+	NamedPipeClient::NamedPipeClient(
+		_In_z_ A_LPCSTR lpName,
+		_In_ PipeOpenMode dwOpenMode,
+		_In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes
+	) : INamedPipeBase(&hPipe), dwOpenMode(dwOpenMode) {
+		if (lpName[0] == '\\' && lpName[1] == '\\' && lpName[2] == '.' && lpName[3] == '\\') strcpy_s(szName, lpName);
+		else sprintf_s(szName, "\\\\.\\%s", lpName);
 
-		if (!ReadFile(
-			hPipe,
-			lpBuffer,
-			dwNumberOfBytesToRead,
-			lpNumberOfBytesRead,
-			nullptr
-		)) AuroraThrow(WindowsApiException, "ReadFile");
-
-		AuroraContextEnd();
+		if (lpSecurityAttributes) {
+			memcpy(&SecurityAttributes, lpSecurityAttributes, sizeof(SECURITY_ATTRIBUTES));
+			bSecurityAttributesActive = true;
+		}
+		else bSecurityAttributesActive = false;
 	}
 
-	A_VOID NamedPipeServer::Read(
-		_Out_writes_z_(dwSize) A_LPSTR lpString,
-		_In_ A_DWORD dwSize
-	) {
+	A_VOID NamedPipeClient::Create() {
 		AuroraContextStart();
 
-		A_LPSTR lpBuffer = new A_CHAR[dwSize];
-		A_DWORD dwNumberOfBytesRead;
-
-		if (!ReadFile(
-			hPipe,
-			lpBuffer,
-			dwSize,
-			&dwNumberOfBytesRead,
-			nullptr
-		)) {
-			delete[] lpBuffer;
-			AuroraThrow(WindowsApiException, "ReadFile");
+		A_DWORD dwDesiredAccess;
+		switch (dwOpenMode) {
+		case PipeOpenMode::Inbound:
+			dwDesiredAccess = GENERIC_READ;
+			break;
+		case PipeOpenMode::Outbound:
+			dwDesiredAccess = GENERIC_WRITE;
+			break;
+		case PipeOpenMode::Duplex:
+			dwDesiredAccess = GENERIC_READ | GENERIC_WRITE;
+			break;
+		default:
+			dwDesiredAccess = 0;
+			break;
 		}
 
-		try { // Try-catch in order to not write null terminator twice.
-			for (A_I32 i = 0; i < dwNumberOfBytesRead - 1; i++) {
-				lpString[i] = lpBuffer[i];
-				if (lpBuffer[i] == '\0') throw 0;
-			}
-
-			lpString[dwNumberOfBytesRead - 1] = '\0';
-		}
-		catch (int) {}
-
-		AuroraContextEnd();
-	}
-
-	A_VOID NamedPipeServer::Write(
-		_In_reads_bytes_(dwNumberOfBytesToWrite) A_LPCVOID lpBuffer,
-		_In_ A_DWORD dwNumberOfBytesToWrite,
-		_Out_opt_ A_LPDWORD lpNumberOfBytesWritten = nullptr
-	) {
-		AuroraContextStart();
-
-		if (!WriteFile(
-			hPipe,
-			lpBuffer,
-			dwNumberOfBytesToWrite,
-			lpNumberOfBytesWritten,
+		hPipe = CreateFileA(
+			szName,
+			dwDesiredAccess,
+			FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+			bSecurityAttributesActive ? &SecurityAttributes : nullptr,
+			OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL,
 			nullptr
-		)) AuroraThrow(WindowsApiException, "WriteFile");
+		);
+
+		if (hPipe == INVALID_HANDLE_VALUE) AuroraThrow(WindowsApiException, "CreateFileA");
 
 		AuroraContextEnd();
-	}
-
-	A_VOID NamedPipeServer::Write(_In_z_ A_LPCSTR lpString) {
-		AuroraContextStart();
-
-		if (!WriteFile(
-			hPipe,
-			lpString,
-			strlen(lpString) + 1,
-			nullptr,
-			nullptr
-		)) AuroraThrow(WindowsApiException, "WriteFile");
-
-		AuroraContextEnd();
-	}
-
-	A_VOID NamedPipeServer::Clone(_Reserved_ A_LPVOID) const { /* throw NotImplementedException */ }
-
-	A_VOID NamedPipeServer::Release() {
-		if (hPipe) {
-			CloseHandle(hPipe);
-			hPipe = nullptr;
-		}
 	}
 }
--- Aurora/Pipe.h
+++ Aurora/Pipe.h
@@ -33,27 +33,12 @@ namespace Aurora {
 
 	struct PipeModeFlags : EnumFlags<PipeMode, A_DWORD> { using EnumFlags::EnumFlags; using enum PipeMode; };
 
-	class NamedPipeServer : public IDisposable {
-		A_CHAR szName[MAX_PATH];
-		PipeOpenMode dwOpenMode;
-		PipeModeFlags dwPipeMode;
-		A_DWORD dwMaxInstances;
-		SECURITY_ATTRIBUTES SecurityAttributes;
-		A_BOOL bSecurityAttributesActive;
-		HANDLE hPipe;
-
+	class INamedPipeBase : public IDisposable {
+		HANDLE* phPipe;
 	public:
-		NamedPipeServer(
-			_In_z_ A_LPCSTR lpName,
-			_In_ PipeOpenMode dwOpenMode,
-			_In_ PipeModeFlags dwPipeMode,
-			_In_range_(1, PIPE_UNLIMITED_INSTANCES) A_DWORD dwMaxInstances = PIPE_UNLIMITED_INSTANCES,
-			_In_ LPSECURITY_ATTRIBUTES lpSecurityAttributes = nullptr
-		);
+		INamedPipeBase(_In_ LPHANDLE lpPipeHandle);
 
-		A_VOID Create();
-		A_VOID Connect() const;
-		A_VOID Disconnect() const;
+		virtual A_VOID Create() = 0;
 
 		A_VOID Read(
 			_Out_writes_bytes_(dwNumberOfBytesToRead) A_LPVOID lpBuffer,
@@ -86,7 +71,7 @@ namespace Aurora {
 		);
 
 		A_VOID Write(_In_z_ A_LPCSTR lpString);
-		
+
 		template<typename T>
 		A_VOID Write(_In_ const T* lpBuffer) {
 			AuroraContextStart();
@@ -106,14 +91,55 @@ namespace Aurora {
 		/// <param name="">This functions parameters are reserved.</param>
 		/// <exception cref="NotImplementedException"/>
 		virtual A_VOID IDisposable::Clone(_Reserved_ A_LPVOID) const;
-		NamedPipeServer(const NamedPipeServer&) = delete;
+		INamedPipeBase(const INamedPipeBase&) = delete;
 
 		virtual A_VOID IDisposable::Release();
-		~NamedPipeServer() { Release(); }
+		~INamedPipeBase();
 	};
 
-	class NamedPipeClient {
+	class NamedPipeServer : public INamedPipeBase {
+		A_CHAR szName[MAX_PATH];
+		PipeOpenMode dwOpenMode;
+		PipeModeFlags dwPipeMode;
+		A_DWORD dwMaxInstances;
+		SECURITY_ATTRIBUTES SecurityAttributes;
+		A_BOOL bSecurityAttributesActive;
+		HANDLE hPipe;
 
+	public:
+		NamedPipeServer(
+			_In_z_ A_LPCSTR lpName,
+			_In_ PipeOpenMode dwOpenMode,
+			_In_ PipeModeFlags dwPipeMode,
+			_In_range_(1, PIPE_UNLIMITED_INSTANCES) A_DWORD dwMaxInstances = PIPE_UNLIMITED_INSTANCES,
+			_In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes = nullptr
+		);
+
+		NamedPipeServer(const NamedPipeServer&) = delete;
+
+		virtual A_VOID INamedPipeBase::Create();
+
+		A_VOID Connect() const;
+		A_VOID Disconnect() const;
+	};
+
+	class NamedPipeClient : public INamedPipeBase {
+		A_CHAR szName[MAX_PATH];
+		PipeOpenMode dwOpenMode;
+		SECURITY_ATTRIBUTES SecurityAttributes;
+		A_BOOL bSecurityAttributesActive;
+		HANDLE hPipe;
+
+	public:
+		NamedPipeClient(
+			_In_z_ A_LPCSTR lpName,
+			_In_ PipeOpenMode dwOpenMode,
+			_In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes
+		);
+
+		NamedPipeClient(const NamedPipeClient&) = delete;
+
+		virtual A_VOID INamedPipeBase::Create();
 	};
 
 	class AnonymousPipe {
